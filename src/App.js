a4210784d741cb5e8f9c1cb8de6e88465e2757b7
Elliot <130664947+ellioht@users.noreply.github.com>
2023-06-30 19:31:07 +0100

    router basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Notifications from "./components/pages/Notifications";
 function App() {
   return (
     <div className="App">
-        <Router>
+        <Router basename="/plant-watering-app-frontend">
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
